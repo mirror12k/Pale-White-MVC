20b00ee134653f2dd0841abdb861a5435fa46d97
mirror <mirror12k@gmail.com>
2017-10-06 00:30:48 -0400

    - implemented a paged list controller boilerplate

--- PaleWhite/ProjectBoilerplate.pm
+++ PaleWhite/ProjectBoilerplate.pm
@@ -33,11 +33,6 @@ model $user_model_name {
 
 
 "
-	# +login
-	# 	salted_sha256 password;
-	# 	string[32] last_login_timestamp;
-	# +registration
-	# 	string[32] registration_timestamp;
 }
 
 
@@ -91,26 +86,93 @@ controller $controller_name {
 
 
 "
-	# +login
-		# action verify_user_login (string[1:255] username, string[1:255] password) {
-		# 	user = model? $user_model_name username=username;
-		# 	if (!user) {
-		# 		return 0;
-		# 	}
+}
 
-		# 	if (user.matches_hashed_field(\"password\", password)) {
-		# 		user.last_login_timestamp = _time;
-		# 		return user;
-		# 	} else {
-		# 		return 0;
-		# 	}
-		# }
 
-	# +registration
-		# action register_user (string[1:255] username, string[1:255] password) {
-		# 	user = create $user_model_name username=username, password=password, registration_timestamp=_time;
-		# 	return user;
-		# }
+sub default_paged_item_controller {
+	my ($item_model_name, $controller_name, %options) = @_;
+	return "
+
+
+controller $controller_name {
+	action list_page (int page, string? order) {
+		offset = page * 20;
+
+		if (order) {
+			page_items = list $item_model_name _limit=20, _offset=offset, _order=order;
+		} else {
+			page_items = list $item_model_name _limit=20, _offset=offset;
+		}
+
+		return page_items;
+	}
+
+	action has_next_page (int page) {
+		offset = (page + 1) * 20;
+
+		model_count = count $item_model_name;
+		return model_count > offset;
+	}
+
+	action has_previous_page (int page) {
+		return page > 0;
+	}
+
+	action last_page {
+		model_count = count $item_model_name;
+
+		validate page_count as int;
+		# round up if there is a remainder
+		if (model_count % 20) {
+			page_count = page_count + 1;
+		}
+
+		return page_count;
+	}" .
+	($options{search} ? "
+
+	# +search
+	action list_search_page (int page, query, string? order) {
+		offset = page * 20;
+
+		query._limit = 20;
+		query._offset = offset;
+		if (order) {
+			query._order = order;
+		}
+
+		page_items = model::$item_model_name.get_list(query);
+
+		return page_items;
+	}
+
+	action has_next_search_page (int page, query) {
+		offset = (page + 1) * 20;
+
+		model_count = model::$item_model_name.count(query);
+		return model_count > offset;
+	}
+
+	action has_previous_search_page (int page) {
+		return page > 0;
+	}
+
+	action last_search_page (query) {
+		model_count = model::$item_model_name.count(query);
+
+		validate page_count as int;
+		# round up if there is a remainder
+		if (model_count % 20) {
+			page_count = page_count + 1;
+		}
+
+		return page_count;
+	}" : '') .
+	"
+}
+
+
+"
 }
 
 sub write_user_controller {
@@ -123,11 +185,21 @@ sub write_user_controller {
 	$dir->new_file("$controller_name.white")->write(default_user_controller($user_model_name, $controller_name, %options));
 }
 
+sub write_paged_list_controller {
+	my ($project_directory, $item_model_name, $controller_name, %options) = @_;
+
+	my $dir = Sugar::IO::Dir->new($project_directory);
+	die "directory $dir doesnt exist" unless $dir->exists;
+
+	$dir->new_file("$controller_name.white")->write(default_paged_item_controller($item_model_name, $controller_name, %options));
+}
+
 sub main {
 
 	die "usage: $0 <options...> <project directory name>\n
 	options:
-		--user-controller <user_model_name> <controller_name> [+login] [+registration]" unless @_;
+		--user-controller <user_model_name> <controller_name> [+login] [+registration]
+		--paged-list-controller <item_model_name> <controller_name> [+search]" unless @_;
 
 	my %options;
 
@@ -139,11 +211,17 @@ sub main {
 			$target = 'user_controller';
 			push @target_args, shift // die "user model name argument required";
 			push @target_args, shift // die "controller name argument required";
+		} elsif ($arg eq '--paged-list-controller') {
+			$target = 'paged_list_controller';
+			push @target_args, shift // die "item model name argument required";
+			push @target_args, shift // die "controller name argument required";
 
 		} elsif ($arg eq '+login') {
 			$options{login} = 1;
 		} elsif ($arg eq '+registration') {
 			$options{registration} = 1;
+		} elsif ($arg eq '+search') {
+			$options{search} = 1;
 
 		} else {
 			die "invalid option: $arg";
@@ -155,6 +233,8 @@ sub main {
 
 	if ($target eq 'user_controller') {
 		write_user_controller($project_directory, @target_args, %options);
+	} elsif ($target eq 'paged_list_controller') {
+		write_paged_list_controller($project_directory, @target_args, %options);
 	} else {
 		...
 	}
